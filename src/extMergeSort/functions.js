a1b7414a59e45482e5d994fec4581e316ce332e7
Marcelo Barros <mhbarros99@gmail.com>
2019-09-18 17:48:19 -0300

    Separação dos arquivos corrigida

--- src/extMergeSort/functions.js
+++ src/extMergeSort/functions.js
@@ -30,7 +30,21 @@ const separaArquivo = (fd, qtdArquivos) => {
 
             fs.readSync(fd, buffer, 0, tamanhoBuffer, qtdPorArquivo * TAM_LINHA * i);
 
-            fs.writeFileSync(nomeArquivo, iconv.encode(buffer.toString('latin1'), 'iso-8859-1'));
+            // Os endereços estão no buffer. Ordena-los
+            let enderecos = quickSort(buffer.toString('latin1'), qtdPorArquivo);
+            let final = '';
+            enderecos.forEach((v) => {
+                let rua    = v.substr(0, 72);
+                let bairro = v.substr(72, 72);
+                let cidade = v.substr(144, 72);
+                let estado = v.substr(216, 72);
+                let sigla  = v.substr(288, 2);
+                let cep    = v.substr(290, 8);
+                final += '\n' + rua + bairro + cidade + estado + sigla + cep;
+
+            });
+            fs.appendFileSync(nomeArquivo, iconv.encode(final, 'iso-8859-1'));
+
 
         } else {
 
@@ -38,60 +52,78 @@ const separaArquivo = (fd, qtdArquivos) => {
             buffer = Buffer.alloc(tamanhoBuffer);
 
             fs.readSync(fd, buffer, 0, TAM_LINHA * qtdPorArquivo + (quebrado * TAM_LINHA), qtdPorArquivo * TAM_LINHA * i);
-            fs.writeFileSync(nomeArquivo, iconv.encode(buffer.toString('latin1'), 'iso-8859-1'));
+
+            let enderecos = quickSort(buffer.toString('latin1'));
+            let final = '';
+            enderecos.forEach((v) => {
+                let rua    = v.substr(0, 72);
+                let bairro = v.substr(72, 72);
+                let cidade = v.substr(144, 72);
+                let estado = v.substr(216, 72);
+                let sigla  = v.substr(288, 2);
+                let cep    = v.substr(290, 8);
+                final += '\n' + rua + bairro + cidade + estado + sigla + cep;
+
+            });
+            fs.appendFileSync(nomeArquivo, iconv.encode(final, 'iso-8859-1'));
         }
     }
     return true;
 };
 
-const quickSort = (fd, numArquivo) => {
-     let buffer = Buffer.alloc(TAM_LINHA);
+const quickSort = (registros) => {
 
-    let statsCep = null;
+    if(typeof registros !== 'undefined'){
+        let enderecos = registros.split('\n');
 
-    try {
-        statsCep = fs.fstatSync(fd);
-    } catch (e) {
-        console.log("Não consegui ler o arquivo cep.dat ... Ele está na pasta /src/data ?");
-        return false;
+        enderecos.sort((a,b) => {
+            let cepa    = a.substr(290, 8);
+            let cepb    = b.substr(290, 8);
+
+            if(parseInt(cepa) < parseInt(cepb)){
+                return -1;
+            }else if(parseInt(cepa) > parseInt(cepb)){
+                return 1;
+            }
+            return 0;
+        });
+
+        enderecos = enderecos.map(i => i + '\n');
+
+        return enderecos;
     }
 
-    let qtdRegistros = statsCep.size / TAM_LINHA;
-
-    let enderecos = [];
-    for(let i = 0 ; i < qtdRegistros ; i++){
-        fs.readSync(fd, buffer, 0, TAM_LINHA, i * TAM_LINHA);
-
-        let end = buffer.toString('latin1');
+    /*for (let i = 0; i < tamArquivo; i++) {
 
         // Jogando para estrutura
-        let rua      = end.substr(0, 72);
-        let bairro   = end.substr(72, 72);
-        let cidade   = end.substr(144, 72);
-        let estado   = end.substr(216, 72);
-        let sigla    = end.substr(288, 2);
-        let cep      = end.substr(290, 8);
+        let rua    = registros.substr(0, 72);
+        let bairro = registros.substr(72, 72);
+        let cidade = registros.substr(144, 72);
+        let estado = registros.substr(216, 72);
+        let sigla  = registros.substr(288, 2);
+        let cep    = registros.substr(290, 8);
 
         enderecos.push({rua, bairro, cidade, estado, sigla, cep});
     }
 
-    enderecos.sort((a,b) => {
-        if(a.cep < b.cep){
+    enderecos.sort((a, b) => {
+        if (a.cep < b.cep) {
             return -1;
-        }else if(b.cep < a.cep){
+        } else if (b.cep < a.cep) {
             return 1;
         }
         return 0;
-    });
+    });*/
 
     // Removendo o arquivo anterior para substituir pelo novo
-    fs.unlinkSync(`./src/extMergeSort/data/cep_${numArquivo}.dat`);
+    /*fs.unlinkSync(`./src/extMergeSort/data/cep_${numArquivo}.dat`);
     fs.closeSync(fd);
 
     enderecos.forEach((v) => {
         let novoEndereco = v.rua + v.bairro + v.cidade + v.estado + v.sigla + v.cep + "\n";
         fs.appendFileSync(`./src/extMergeSort/data/cep_${numArquivo}.dat`, novoEndereco);
-    });
+    });*/
+    return enderecos;
 };
 
 const merge = (fd) => {
